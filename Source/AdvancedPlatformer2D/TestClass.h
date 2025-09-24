0f8a23985eb0ba91fdf1c4b785fe891721a64298
주후산 <microbin98@gmail.com>
2025-09-24 17:36:03 +0900

    20250924
    Project Creation and Initial Setup

---
+++ Source/AdvancedPlatformer2D/TestClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ADVANCEDPLATFORMER2D_API TestClass
+{
+public:
+	TestClass();
+	~TestClass();
+};
